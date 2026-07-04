238382b15d56fcc822e691b6d82860305cdf3d73
x47-cmd <x_47@hotmail.com>
2026-07-04 08:41:11 +0400

    Update achievements.js

--- js/achievements.js
+++ js/achievements.js
@@ -1,1 +1,17 @@
-// Achievements module
+function unlockAchievement(id,title,icon){
+
+  let achievements=JSON.parse(localStorage.getItem("achievements")||"[]");
+
+  if(achievements.includes(id)) return;
+
+  if(navigator.vibrate){
+    navigator.vibrate([150,80,150]);
+}
+
+alert(
+"🎉🏆 إنجاز جديد!\n\n"
++icon+" "+title+
+"\n\nتم فتح الإنجاز بنجاح 🎊"
+);
+
+}
